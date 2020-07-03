d937a6582be2f1429ee96cd2c5e47f1b4c7fdb3a
cleytonRR <cleytonrodrigues69@live.com>
2020-07-02 23:55:41 -0300

    Created method for router protected

--- src/app/controllers/UserController.ts
+++ src/app/controllers/UserController.ts
@@ -4,6 +4,10 @@ import { getRepository } from "typeorm";
 import User from "../models/User";
 
 class UserController {
+  index(req: Request, res: Response) {
+    return res.send("Ok");
+  }
+
   async store(req: Request, res: Response) {
     const repository = getRepository(User);
 
